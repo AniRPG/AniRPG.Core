2d0b37ff0a424d7f705be6d095c37b8f6f25fbd1
aangairbender <jetixranger@ya.ru>
2019-07-20 19:20:49 +0300

    IEntity and some common stuff added

---
+++ AniRPG.Core.Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AniRPG.Core.Domain;
+
+namespace AniRPG.Core.Common.Exceptions
+{
+    public abstract class EntityNotFoundException : Exception
+    {
+        public int EntityId { get; }
+
+        public string EntityName { get; }
+
+        protected EntityNotFoundException(string name, int id)
+            : base("Entity {name}(Id = {id}) not found.")
+        {
+            EntityName = name;
+            EntityId = id;
+        }
+    }
+
+    public class EntityNotFoundException<T> : EntityNotFoundException
+        where T : IEntity
+    {
+        public EntityNotFoundException(int id)
+            : base(nameof(T), id)
+        {
+        }
+    }
+}
---
+++ AniRPG.Core.Common/Repositories/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using AniRPG.Core.Domain;
+
+namespace AniRPG.Core.Common.Repositories
+{
+    public interface IEntityRepository<T>
+        where T : IEntity
+    {
+        Task AddEntity(T entity);
+        Task DeleteEntity(int entityId);
+        Task UpdateEntity(T entityId);
+        Task<T> GetEntity(int entityId);
+    }
+}
